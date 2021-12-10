226ff3780ddcef08f038d4502b03fdf7e1cf4e17
dhrubajyoti98 <dhrubajyoti98@gmail.com>
2021-12-10 22:22:45 +0530

    Cleaning Up

--- src/main_driver.cpp
+++ src/main_driver.cpp
@@ -28,40 +28,39 @@ int main(int argc, char* argv[])
 
     int RUN_COUNTER;
     int NUM_RUNS=(int)atof(argv[1]);
+    
+    if(NUM_RUNS>999)printf("Large number of passes. Might take long.\n");
 
     double d_integral_time=0, m_integral_time=0;
     double lineq_time=0, ode_time=0;
-    double total_time=0;
     double flops=0;
 
     for(RUN_COUNTER=1;RUN_COUNTER<=NUM_RUNS;RUN_COUNTER++)
     {
         printf("Running Sample %03d of %d.\n",RUN_COUNTER,NUM_RUNS);
 
-        // printf("Running DGEMM...\n");
+        printf("Running DGEMM...\n");
+        flops=flops+matrix_mul_flops_timer(h);
+        
+        printf("Direct Integral-Trapezoidal Rule, Evaluating Pi.\n");
+        d_integral_time+=direct_integral_timer();
 
-        // printf("Direct Integral-Trapezoidal Rule, Evaluating Pi...\n");
-        // d_integral_time+=direct_integral_timer();
+        printf("Monte-Carlo Integral, Evaluating Pi.\n");
+        m_integral_time+=mc_integral_timer(h);
 
-        // printf("Monte-Carlo Integral, Evaluating Pi...\n");
-        // m_integral_time+=mc_integral_timer(h);
+        printf("Solving Linear Equations using Gauss-Seidel Method.\n");
+        lineq_time+=lineq_timer(h);
 
-        // printf("Solving Linear Equations using Gauss-Seidel Method...\n");
-        // lineq_time+=lineq_timer(h);
-
-        // printf("Solving a high-dimensional ODE using RK4...\n");
-        // ode_time+=ode_timer(h);
-
-        // printf("\n");
-        // total_time+=(d_integral_time+m_integral_time+lineq_time+ode_time);
+        printf("Solving a high-dimensional ODE using RK4.\n");
+        ode_time+=ode_timer(h);
     }
-
+    printf("\n");
     printf("--------------------------Results-for-%03d-PASSES--------------------------\n",NUM_RUNS);
-    printf("| DGEMM                              | %14.4lf Avg. MFLOPS        |\n",flops);
-    printf("| Monte-Carlo Integral               | %14.4lf seconds/pass       |\n",m_integral_time);
-    printf("| Trapezoidal Integral               | %14.4lf seconds/pass       |\n",d_integral_time);
-    printf("| ODE Solver                         | %14.4lf seconds/pass       |\n",ode_time);
-    printf("| Gauss Seidel Iteration             | %14.4lf seconds/pass       |\n",lineq_time);
+    printf("| DGEMM                              | %14.4lf Avg. MFLOPS        |\n",flops/NUM_RUNS);
+    printf("| Monte-Carlo Integral               | %14.4lf seconds/pass       |\n",m_integral_time/NUM_RUNS);
+    printf("| Trapezoidal Integral               | %14.4lf seconds/pass       |\n",d_integral_time/NUM_RUNS);
+    printf("| ODE Solver                         | %14.4lf seconds/pass       |\n",ode_time/NUM_RUNS);
+    printf("| Gauss Seidel Iteration             | %14.4lf seconds/pass       |\n",lineq_time/NUM_RUNS);
     printf("--------------------------------------------------------------------------\n");
 
     return 0;
